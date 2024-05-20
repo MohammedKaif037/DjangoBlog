b8be7f7dd9c2a51acca5c8ff81e639af5b9d7ccb
MohammedKaif037 <kaifmohammed037@gmail.com>
2024-05-20 05:43:39 +0530

    richtext updattion

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from ckeditor.fields import RichTextField  # Assuming you are using CKEditor for rich text editing
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -8,30 +9,39 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('category-detail', args=[str(self.id)])
-
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('tag-detail', args=[str(self.id)])
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     summary = models.TextField()
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
-    featured_image = models.ImageField(upload_to='featured_images/')
+    featured_image = models.ImageField(upload_to='featured_images/', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
-
     def get_absolute_url(self):
         return reverse('post-detail', args=[str(self.id)])
+
+class Subheading(models.Model):
+    post = models.ForeignKey(Post, related_name='subheadings', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = RichTextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class CodeBlock(models.Model):
+    post = models.ForeignKey(Post, related_name='code_blocks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    code = models.TextField()
+
+    def __str__(self):
+        return self.title if self.title else f"Code Block {self.id}"
